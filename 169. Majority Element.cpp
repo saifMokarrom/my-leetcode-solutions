35d664275d229698b8e85d7a5a89bfffbbb43778
Saif Mokarrom <46572647+saifMokarrom@users.noreply.github.com>
2022-08-08 21:27:55 +0600

    Update 169. Majority Element.cpp

--- 169. Majority Element.cpp
+++ 169. Majority Element.cpp
@@ -23,15 +23,16 @@ moors voting algorithm
 public class Solution {
     public int majorityElement(int[] num) {
 
-        int major=num[0], count = 1;
-        for(int i=1; i<num.length;i++){
-            if(count==0){
-                count++;
-                major=num[i];
-            }else if(major==num[i]){
+        int major=nums[0], count = 1;
+        for(int i=1; i<nums.size();i++){
+            
+            if(major==nums[i]){
                 count++;
             }else count--;
-            
+            if(count==0){
+                count++;
+                major=nums[i];
+            }
         }
         return major;
     }
