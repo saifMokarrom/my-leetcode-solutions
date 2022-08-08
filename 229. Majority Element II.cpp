5b83ec20f740203fda9295593c1549b59e45eb82
Saif Mokarrom <46572647+saifMokarrom@users.noreply.github.com>
2022-08-08 19:38:34 +0600

    Create 229. Majority Element II.cpp

---
+++ 229. Majority Element II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int,int> map;
+        vector<int> ans;
+        for (auto it: nums){
+            map[it]++;
+        }
+        
+        for (auto it: map){
+            if (it.second > (nums.size()/3)) { ans.push_back(it.first);}
+        }
+        
+        return ans;
+    }
+};
