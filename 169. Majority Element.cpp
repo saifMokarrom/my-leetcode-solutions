526765110bdeb1aa1a4e7ed9be1373b459e5b6db
Saif Mokarrom <46572647+saifMokarrom@users.noreply.github.com>
2022-08-08 20:43:41 +0600

    Update 169. Majority Element.cpp

--- 169. Majority Element.cpp
+++ 169. Majority Element.cpp
@@ -13,3 +13,27 @@ public:
         return -1;
     }
 };
+
+
+/*
+O(n) and space complexity: 0(1)
+moors voting algorithm
+
+
+public class Solution {
+    public int majorityElement(int[] num) {
+
+        int major=num[0], count = 1;
+        for(int i=1; i<num.length;i++){
+            if(count==0){
+                count++;
+                major=num[i];
+            }else if(major==num[i]){
+                count++;
+            }else count--;
+            
+        }
+        return major;
+    }
+}
+*/
